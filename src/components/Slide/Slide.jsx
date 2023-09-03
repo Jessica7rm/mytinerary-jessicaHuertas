51f04828c99afa76f3e588731701cde59b19b777
Jessica <jessica7rm@gmail.com>
2023-09-03 11:29:00 -0300

    cambio a redux y base de datos al store carousel

--- src/components/Slide/Slide.jsx
+++ src/components/Slide/Slide.jsx
@@ -1,19 +1,35 @@
 import './styles.css'
 import CardHome from '../CardHome'
-import Data from '../../Data/data.json'
+// import Data from '../../Data/data.json'
+import { useEffect } from 'react';
+import { getAllCities } from '../../service/citiesQueries.js';
+import { useDispatch, useSelector } from 'react-redux'
+import getCitiesAction from '../../store/actions/citiesActions';
+
 
 function Slide({ slide }) {
+
+    let citiesInStore = useSelector(store => store.citiesReducers.cities)
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        getAllCities().then(data => {
+          dispatch(getCitiesAction.get_cities(data))
+        })
+        .catch(err => console.log(err))
+      }, [slide]);
+
     let arrList = []
 
     let arraySelect = () => {
-        arrList = Data.slice(slide * 4, (slide + 1) * 4)
+        arrList = citiesInStore?.slice(slide * 4, (slide + 1) * 4)
 
     }
 
     arraySelect()
     return (
         <div className="row row-cols-1 row-cols-md-2 row-cols-sm-2  g-4">
-            {arrList.map((elem) => (<CardHome key={elem.id} src={elem.src} place={elem.place} country={elem.country} />))}
+            {arrList.map((elem) => (<CardHome key={elem._id} src={elem.image} place={elem.place} country={elem.country} />))}
         </div>
     )
 }
